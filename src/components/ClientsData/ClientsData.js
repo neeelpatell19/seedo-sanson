556311adb22f0b4f1bd04f92b58c4db55283a498
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-09-25 12:56:53 +0530

    Updated Data

--- src/components/ClientsData/ClientsData.js
+++ src/components/ClientsData/ClientsData.js
@@ -2,98 +2,98 @@ const ClientsData = [
     {
         id: 1,
         name: "Amazon",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/AmazonLogo.png"
+        image: "/Images/ClientsLogos/amazon.png"
     },
     {
         id: 2,
         name: "Flipkart",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/FlipkartLogo.png"
+        image: "/Images/ClientsLogos/Flipkart.png"
     },
     {
         id: 3,
         name: "Hamleys",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/HamleysLogo.png"
+        image: "/Images/ClientsLogos/hamleys.png"
     },
-    {
-        id: 4,
-        name: "Toys R Us",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/ToysRusLogo.png"
-    },
-    {
-        id: 5,
-        name: "DMart",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/DmartLogo.png"
-    },
+    // {
+    //     id: 4,
+    //     name: "Toys R Us",
+    //     image: "/Images/ClientsLogos/ToysRusLogo.png"
+    // },
+    // {
+    //     id: 5,
+    //     name: "DMart",
+    //     image: "/Images/ClientsLogos/DMartLogo.png"
+    // },
     {
         id: 6,
         name: "LuLu",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/LuLuLogo.png"
+        image: "/Images/ClientsLogos/LuLu.png"
     },
     {
         id: 7,
         name: "Blinkit",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/BlinkitLogo.png"
+        image: "/Images/ClientsLogos/ Blinkit.png"
     },
-    {
-        id: 8,
-        name: "Zepto",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/ZeptoLogo.png"
-    },
+    // {
+    //     id: 8,
+    //     name: "Zepto",
+    //     image: "/Images/ClientsLogos/ZeptoLogo.png"
+    // },
     {
         id: 9,
         name: "Instamart",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/InstamartLogo.png"
+        image: "/Images/ClientsLogos/Instamart.png"
     },
     {
         id: 10,
         name: "Smart Superstore",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/SmartSuperstoreLogo.png"
+        image: "/Images/ClientsLogos/Smart.png"
     },
-    {
-        id: 11,
-        name: "MR DIY",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/MRDIYLogo.png"
-    },
-    {
-        id: 12,
-        name: "Mega Mart Market",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/MegaMartMarketLogo.png"
-    },
-    {
-        id: 13,
-        name: "24Seven",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/24SevenLogo.png"
-    },
+    // {
+    //     id: 11,
+    //     name: "MR DIY",
+    //     image: "/Images/ClientsLogos/MRDIYLogo.png"
+    // },
+    // {
+    //     id: 12,
+    //     name: "Mega Mart Market",
+    //     image: "/Images/ClientsLogos/MegaMartMarketLogo.png"
+    // },
+    // {
+    //     id: 13,
+    //     name: "24Seven",
+    //     image: "/Images/ClientsLogos/24SevenLogo.png"
+    // },
     {
         id: 14,
         name: "Wishkey",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/WishkeyLogo.png"
+        image: "/Images/ClientsLogos/Wishkey.png"
     },
     {
         id: 15,
         name: "Snooplay",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/SnooplayLogo.png"
+        image: "/Images/ClientsLogos/Snooplay.png"
     },
     {
         id: 16,
         name: "MuMuso",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/MuMusoLogo.png"
+        image: "/Images/ClientsLogos/Mumuso.png"
     },
     {
         id: 17,
         name: "WemBleys",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/WemBleysLogo.png"
+        image: "/Images/ClientsLogos/Wembley.png"
     },
     {
         id: 18,
         name: "Mini",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/MiniLogo.svg"
+        image: "/Images/ClientsLogos/Miniso.png"
     },
-    {
-        id: 19,
-        name: "Rallerzy",
-        image: "/src/components/ClientsData/Logos/ClientsLogos2/Rallerzylogo.svg"
-    }
+    // {
+    //     id: 19,
+    //     name: "Rallerzy",
+    //     image: "/Images/ClientsLogos/Rallerzylogo.svg"
+    // }
 ]
 
 export default ClientsData;
